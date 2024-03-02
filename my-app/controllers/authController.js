402d07f4448a15eb940c37540ab1e35fdb161526
Afifudin Maarif <32411703+afifudinmtop@users.noreply.github.com>
2024-03-02 11:08:31 +0700

    v.1.10.0
    auth api

---
+++ my-app/controllers/authController.js
@@ -0,0 +1,98 @@
+const { pool } = require("../utils/mysql");
+const bcrypt = require("bcryptjs");
+const { v4: uuidv4 } = require("uuid");
+
+const login = async (req, res) => {
+  try {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // check existing username
+    pool.query(
+      "SELECT * FROM user WHERE username = ?",
+      [username],
+      (error, results, fields) => {
+        if (results.length < 1) {
+          return res.json({ pesan: "user not found!" });
+        } else {
+          const resultx = bcrypt.compareSync(password, results[0].password);
+          if (resultx) {
+            req.session.user = { uuid: results[0].uuid, isLoggedIn: true };
+            return res.json({ pesan: "sukses!" });
+          } else {
+            return res.json({ pesan: "wrong password!" });
+          }
+        }
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error");
+  }
+};
+
+const register = async (req, res) => {
+  try {
+    const uuid = uuidv4();
+    const nama = req.body.nama;
+    const email = req.body.email;
+    const username = req.body.username;
+    const password = req.body.password;
+    const hash = bcrypt.hashSync(password, 10);
+
+    // check existing username
+    pool.query(
+      "SELECT * FROM user WHERE username = ? AND email = ?",
+      [username, email],
+      (error, results, fields) => {
+        if (results.length > 0) {
+          res.json({ pesan: "username or email exist!" });
+        } else {
+          // Simpan data ke MySQL
+          pool.query(
+            "INSERT INTO user SET ?",
+            {
+              uuid: uuid,
+              nama: nama,
+              email: email,
+              username: username,
+              password: hash,
+              gambar: "avatar.png",
+            },
+            (error, results, fields) => {
+              if (error) throw error;
+              res.json({ pesan: "sukses!" });
+            }
+          );
+        }
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error");
+  }
+};
+
+const logout = (req, res) => {
+  req.session.destroy(); // Menghapus sesi
+  res.redirect("/login");
+};
+
+const session = (req, res) => {
+  res.json({ user: req.session.user || null });
+};
+
+const session_check = (req, res) => {
+  if (req.session.user) {
+    return res.json({ isLoggedIn: true });
+  }
+  return res.json({ isLoggedIn: false });
+};
+
+module.exports = {
+  login,
+  register,
+  logout,
+  session,
+  session_check,
+};
--- my-app/server.js
+++ my-app/server.js
@@ -13,6 +13,7 @@ const cors = require("cors");
 
 // import routes
 const tesRoutes = require("./routes/tesRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 nextApp.prepare().then(() => {
   const app = express();
@@ -33,6 +34,7 @@ nextApp.prepare().then(() => {
 
   // use Routes
   app.use("/api/tes", tesRoutes);
+  app.use("/api/auth", authRoutes);
 
   // Semua request lain akan di-handle oleh Next.js
   app.get("*", (req, res) => {

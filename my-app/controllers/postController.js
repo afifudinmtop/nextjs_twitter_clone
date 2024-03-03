f0a051415fd06476b42476d06a0d47f68c38efa8
Afifudin Maarif <32411703+afifudinmtop@users.noreply.github.com>
2024-03-03 09:19:29 +0700

    v.1.18.0
    new post kalau gk ada gambar

--- my-app/controllers/postController.js
+++ my-app/controllers/postController.js
@@ -3,28 +3,34 @@ const sharp = require("sharp");
 const { pool } = require("../utils/mysql");
 const { uploadDir } = require("../utils/uploadDir");
 const { renameFile } = require("../utils/renameFile");
+const { v4: uuidv4 } = require("uuid");
 
 const upload_gambar = async (req, res) => {
   try {
     const caption = req.body.caption;
     const file = req.file;
-    const resizedFilename = file.filename;
+    let resizedFilename = "";
 
-    // Resize gambar
-    await sharp(file.path)
-      .resize(375, 375)
-      .toFile(`${uploadDir}/x-${resizedFilename}`);
+    // Jika ada file yang di-upload, maka resize dan update nama file
+    if (file) {
+      resizedFilename = file.filename;
 
-    renameFile(
-      `${uploadDir}/x-${resizedFilename}`,
-      `${uploadDir}/${resizedFilename}`
-    );
+      // Resize gambar
+      await sharp(file.path)
+        .resize(375, 375)
+        .toFile(`${uploadDir}/x-${resizedFilename}`);
+
+      renameFile(
+        `${uploadDir}/x-${resizedFilename}`,
+        `${uploadDir}/${resizedFilename}`
+      );
+    }
 
     // Simpan data ke MySQL
     pool.query(
       "INSERT INTO post SET ?",
       {
-        uuid: resizedFilename.split(".")[0],
+        uuid: file ? resizedFilename.split(".")[0] : uuidv4(),
         gambar: resizedFilename,
         caption: caption,
         user: req.session.user.uuid,
